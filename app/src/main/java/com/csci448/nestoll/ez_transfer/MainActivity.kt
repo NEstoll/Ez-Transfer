f641a5596b179fa4a826b599177128e256de79ba
Nicholas Estoll <nicholas.estoll@gmail.com>
2023-03-04 16:32:02 -0700

    First commit

---
+++ app/src/main/java/com/csci448/nestoll/ez_transfer/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.csci448.nestoll.ez_transfer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.csci448.nestoll.ez_transfer.ui.theme.EzTransferTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EzTransferTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    EzTransferTheme {
+        Greeting("Android")
+    }
+}
